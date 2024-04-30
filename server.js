9396eb78b071e267417ef8dadeab4750a334f929
Dharani Nuthula <dharanireddy952@gmail.com>
2024-04-30 23:44:53 +1000

    initial commit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', router);
 
 
-const PORT = process.env.PORT || 5501;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
